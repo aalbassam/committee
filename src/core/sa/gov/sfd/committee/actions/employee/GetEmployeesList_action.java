ff601ada98cb8afa4099587c723d427eb6f41251
aalbassam <a.albassam@sfd.gov.sa>
2020-10-13 11:23:19 +0300

    update actions

--- src/core/sa/gov/sfd/committee/actions/employee/GetEmployeesList_action.java
+++ src/core/sa/gov/sfd/committee/actions/employee/GetEmployeesList_action.java
@@ -1,15 +1,5 @@
-package sa.gov.sfd.committee.actions.employee;
+package core.sa.gov.sfd.committee.actions.employee;
 
 public class GetEmployeesList_action {
 
-       /* private final EmployeeServices employeeServices;
-
-
-        public GetEmployeesList(EmployeeServices employeeServices) {
-            this.employeeServices = employeeServices;
-        }
-
-        public List<EmployeeEntity> getEmployeesList(){
-            return this.employeeServices.getEmployeesList();
-        }*/
 }
